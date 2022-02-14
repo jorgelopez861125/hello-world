bbd4d071f30f1415182100270c2f31ec7edc978a
jorge <fabricio861125@gmail.com>
2022-02-14 12:34:12 -0300

    terminado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [msj, setMsj] = useState ('');
+  
+    useEffect(() => {
+      setMsj('¡Hello World! 😄');
+    })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1 className='mensaje1'>Hello World App</h1>
+      <p className='mensaje2'>{msj}</p>
     </div>
   );
 }
